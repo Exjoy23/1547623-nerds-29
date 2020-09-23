9f45deb14fb02d5f5d4aa5dfec80086eaa92c241
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-09-23 22:56:58 +0300

    Сделал сетку для каталога

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@ let slideTwo = document.querySelector('.slide2');
 let slideThree = document.querySelector('.slide3');
 
 function slider1() {
-    slideOne.classList.remove('hidden');
-    slideTwo.classList.add('hidden');
-    slideThree.classList.add('hidden');
+  slideOne.classList.remove('hidden');
+  slideTwo.classList.add('hidden');
+  slideThree.classList.add('hidden');
 }
 
 function slider2() {
-    slideOne.classList.add('hidden');
-    slideTwo.classList.remove('hidden');
-    slideThree.classList.add('hidden');
+  slideOne.classList.add('hidden');
+  slideTwo.classList.remove('hidden');
+  slideThree.classList.add('hidden');
 }
 
 function slider3() {
-    slideOne.classList.add('hidden');
-    slideTwo.classList.add('hidden');
-    slideThree.classList.remove('hidden')
+  slideOne.classList.add('hidden');
+  slideTwo.classList.add('hidden');
+  slideThree.classList.remove('hidden')
+}
+
+let pageId = document.querySelector("[data-id-page]").getAttribute("data-id-page"),
+  navItem = document.querySelector(`[data-id-nav=${pageId}]`);
+
+if (pageId == navItem.getAttribute("data-id-nav")) {
+  navItem.classList.add("current__link");
 }
