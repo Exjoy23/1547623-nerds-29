bf43bd8009e356685a59e852bd5e679478d98623
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-10-03 17:10:28 +0300

    Внес правки по оформлению

--- js/script.js
+++ js/script.js
@@ -1,48 +1,45 @@
 'use strict';
 
-function getShow(selector) {
+function getShow(selector, selector1) {
   document.querySelector(selector).classList.add('current');
+  if (selector1) {
+    document.querySelector(selector1).classList.add('current');
+  }
 }
 
-function getHide(selector) {
-  document.querySelector(selector).classList.remove('current');
+function removeCurrent(doc) {
+  doc.forEach(item => item.classList.remove('current'));
 }
 
-function slider1() {
-  getShow('.advantages__slider__button:nth-child(1)');
-  getHide('.advantages__slider__button:nth-child(2)');
-  getHide('.advantages__slider__button:nth-child(3)');
-  getShow('.advantages__item:nth-child(1)');
-  getHide('.advantages__item:nth-child(2)');
-  getHide('.advantages__item:nth-child(3)');
+function getHide(selector, selector1) {
+  removeCurrent(document.querySelectorAll(selector));
+  if (selector1) {
+    removeCurrent(document.querySelectorAll(selector1));
+  }
 }
 
-function slider2() {
-  getShow('.advantages__slider__button:nth-child(2)');
-  getHide('.advantages__slider__button:nth-child(1)');
-  getHide('.advantages__slider__button:nth-child(3)');
-  getShow('.advantages__item:nth-child(2)');
-  getHide('.advantages__item:nth-child(1)');
-  getHide('.advantages__item:nth-child(3)');
+if (document.querySelector('#index')) {
+  const button1 = document.querySelector('#button1');
+  button1.addEventListener('click', () => {
+    getHide('.advantages__slider__button', '.advantages__item');
+    getShow('.advantages__slider__button:nth-child(1)',
+      '.advantages__item:nth-child(1)');
+  });
+
+  const button2 = document.querySelector('#button2');
+  button2.addEventListener('click', () => {
+    getHide('.advantages__slider__button', '.advantages__item');
+    getShow('.advantages__slider__button:nth-child(2)',
+      '.advantages__item:nth-child(2)');
+  });
+
+  const button3 = document.querySelector('#button3');
+  button3.addEventListener('click', () => {
+    getHide('.advantages__slider__button', '.advantages__item');
+    getShow('.advantages__slider__button:nth-child(3)',
+      '.advantages__item:nth-child(3)');
+  });
 }
-
-function slider3() {
-  getShow('.advantages__slider__button:nth-child(3)');
-  getHide('.advantages__slider__button:nth-child(2)');
-  getHide('.advantages__slider__button:nth-child(1)');
-  getShow('.advantages__item:nth-child(3)');
-  getHide('.advantages__item:nth-child(2)');
-  getHide('.advantages__item:nth-child(1)');
-}
-
-const button1 = document.querySelector('#button1');
-button1.addEventListener('click', () => slider1());
-
-const button2 = document.querySelector('#button2');
-button2.addEventListener('click', () => slider2());
-
-const button3 = document.querySelector('#button3');
-button3.addEventListener('click', () => slider3());
 
 const buttonOpenPopup = document.querySelector('#open__popup');
 buttonOpenPopup.addEventListener('click', () => getShow('.contacts__popup'));
