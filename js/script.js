b3e0dd15bfe618b8a7218f9031680a2b5577cf62
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-10-07 23:09:01 +0300

    переделал товары в каталоге

--- js/script.js
+++ js/script.js
@@ -42,7 +42,10 @@ if (document.querySelector('#index')) {
 }
 
 const buttonOpenPopup = document.querySelector('#open__popup');
-buttonOpenPopup.addEventListener('click', () => getShow('.contacts__popup'));
+buttonOpenPopup.addEventListener('click', (event) => {
+  event.preventDefault();
+  getShow('.contacts__popup');
+});
 
 const buttonClosePopup = document.querySelector('#close__popup');
 buttonClosePopup.addEventListener('click', () => getHide('.contacts__popup'));
