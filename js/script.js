c36f63af6a5ac96cc175d7872332f0a5b444a045
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-10-08 22:57:04 +0300

    Переделал открытие/закрытие слайдера и попапа
    Мелкие правки по страницам + товары больше не "прыгают".

--- js/script.js
+++ js/script.js
@@ -18,12 +18,25 @@ function getHide(selector, selector1) {
   }
 }
 
+function addVisuallyHidden(selector, selector1) {
+  selector.classList.add('visually__hidden');
+  if (selector1) {
+    selector1.classList.add('visually__hidden');
+  }
+}
+
+function removeVisuallyHidden(selector) {
+  selector.classList.remove('visually__hidden');
+}
+
 if (document.querySelector('#index')) {
+  const sliderItem = document.querySelectorAll('.advantages__item');
   const button1 = document.querySelector('#button1');
   button1.addEventListener('click', () => {
-    getHide('.advantages__slider__button', '.advantages__item');
-    getShow('.advantages__slider__button:nth-child(1)',
-      '.advantages__item:nth-child(1)');
+    getHide('.advantages__slider__button');
+    getShow('.advantages__slider__button:nth-child(1)');
+    removeVisuallyHidden(sliderItem[0]);
+    addVisuallyHidden(sliderItem[1], sliderItem[2]);
   });
 
   const button2 = document.querySelector('#button2');
@@ -31,6 +44,8 @@ if (document.querySelector('#index')) {
     getHide('.advantages__slider__button', '.advantages__item');
     getShow('.advantages__slider__button:nth-child(2)',
       '.advantages__item:nth-child(2)');
+    removeVisuallyHidden(sliderItem[1]);
+    addVisuallyHidden(sliderItem[0], sliderItem[2]);
   });
 
   const button3 = document.querySelector('#button3');
@@ -38,14 +53,58 @@ if (document.querySelector('#index')) {
     getHide('.advantages__slider__button', '.advantages__item');
     getShow('.advantages__slider__button:nth-child(3)',
       '.advantages__item:nth-child(3)');
+    removeVisuallyHidden(sliderItem[2]);
+    addVisuallyHidden(sliderItem[0], sliderItem[1]);
   });
 }
 
 const buttonOpenPopup = document.querySelector('#open__popup');
 buttonOpenPopup.addEventListener('click', (event) => {
   event.preventDefault();
-  getShow('.contacts__popup');
+  document.querySelector(
+    '.contacts__popup').classList.remove('visually__hidden');
 });
 
 const buttonClosePopup = document.querySelector('#close__popup');
-buttonClosePopup.addEventListener('click', () => getHide('.contacts__popup'));
+buttonClosePopup.addEventListener('click', () => {
+  document.querySelector(
+    '.contacts__popup').classList.add('visually__hidden');
+});
+
+if (document.querySelector('#catalog')) {
+  const btns = document.querySelectorAll('.goods__pagination__link');
+  btns.forEach((item, i) => {
+    btns[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      btns.forEach((items, ii) => {
+        btns[ii].classList.remove('current');
+      });
+      btns[i].classList.add('current');
+    });
+  });
+
+  const btnNext = document.querySelector('.goods__next');
+  btnNext.addEventListener('click', (event) => {
+    event.preventDefault();
+    let count;
+    btns.forEach((item, i) => {
+      const currentLink = btns[i].classList.contains('current');
+      if (currentLink) {
+        count = i + 1;
+      }
+      btns[i].classList.remove('current');
+    });
+    if (count < btns.length) {
+      btns[count].classList.add('current');
+    } else {
+      btns[btns.length - 1].classList.add('current');
+    }
+  });
+
+  const goodsPopupLinks = document.querySelectorAll('.goods__popup__link');
+  goodsPopupLinks.forEach((item, i) => {
+    goodsPopupLinks[i].addEventListener('click', (event) => {
+      event.preventDefault();
+    });
+  });
+}
